1a2acdb6166ded9e35c11c30db2c852068b0050b
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-02-25 23:45:45 +0100

    Codility Performance issue :( 95% 4 tasks, 4th task performance 50%

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -62,8 +62,50 @@ void print(std::vector <int> const& a) {
 		std::cout << a.at(i) << ' ';
 }
 
+unsigned int CodilityPerformance(std::vector<int> blocks)
+{
+	unsigned int front = 0;
+	unsigned int back = 0;
+	unsigned int max = 0;
+	for (int i = 0; i < blocks.size()-1; i++)
+	{
+		if (blocks[i] < blocks[i + 1])
+		{
+			front++;
+			if (max < front + back)
+				max = front + back;
+		}
+		else if (blocks[i] > blocks[i + 1])
+		{
+			if (max < front + back)
+				max = front + back;
+
+			if (front) {
+				front = 0;
+				back=0;
+			}
+			back++;
+			if (max < front + back)
+				max = front + back;
+			
+		}
+		else // (blocks[i] == blocks[i + 1])
+		{
+			back++;
+			if (max < front + back)
+				max = front + back;
+		}
+	}
+	return max;
+}
 int main()
 {
-	std::vector<int> blocks{ 1,4,5,7,4,2,3,6,8,5,2,3,5,6,7 };
+	std::vector<int> blocks1{ 1,4,5,7,4,2,3,6,8,5,2,3,5,6,7 };
+	std::vector<int> blocks2{ 1,2,3,4,5,6,7,8 };
+	std::vector<int> blocks3{ 8,7,6,5,4,3,2,1 };
+	std::vector<int> blocks4{ 1,1,1,1,1,1,1,1,1 };
+	std::vector<int> blocks5{ 1,1 };
+	unsigned int result = CodilityPerformance(blocks5);
+	log(result+1);
 
 }
